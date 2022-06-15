e22b97b38ecc26c5809003edbee768dbdae366a9
Rudder <ben@ruddernation.com>
2022-06-15 19:03:07 +0100

    Original JavaScript Files.

---
+++ canvas/ball2/js/index.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Error 404</title>
+
+<!-- Internet Explorer HTML5 enabling script: -->
+
+<!--[if IE]>
+    <script src="//html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+<![endif]-->
+
+<link rel="stylesheet" type="text/css" href="../../../../css/errorpages.css" />
+
+</head>
+
+<body>
+
+<div id="rocket"></div>
+
+<hgroup>
+    <h1>Page Not Found</h1>
+    <h2>We couldn't find what you were looking for.</h2>
+    <h1><a href="/">Home</a></h1>
+</hgroup>
+<script src="../../../../js/jquery/jquery-latest.min.js"></script>
+<script src="../../../../js/script.js"></script>
+</body>
+</html>
